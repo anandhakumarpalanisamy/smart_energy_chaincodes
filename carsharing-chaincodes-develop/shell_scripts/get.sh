0008b527f37ce71f9e414ae37e235814ad638ae0
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-01-26 21:35:46 +0100

    modified shell scripts

--- carsharing-chaincodes-develop/shell_scripts/get.sh
+++ carsharing-chaincodes-develop/shell_scripts/get.sh
@@ -8,4 +8,4 @@ export CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS
 export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
 export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
 
-peer chaincode query -C appchannel -n carcc -c '{"Args":["listCars"]}'
+peer chaincode query -C appchannel -n energycc -c '{"Args":["listUsers"]}'
--- carsharing-chaincodes-develop/shell_scripts/put.sh
+++ carsharing-chaincodes-develop/shell_scripts/put.sh
@@ -9,7 +9,7 @@ export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
 export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
 
 
-peer chaincode invoke -C appchannel -n carcc -c '{"Args":["createCar","NO9276","Opel","Corsa","Blue","5","2019","1"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
-peer chaincode invoke -C appchannel -n carcc -c '{"Args":["createCar","NO9277","Jaguar","ftype","Black","5","2019","1"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
-peer chaincode invoke -C appchannel -n carcc -c '{"Args":["createCar","NO9278","Dacia","Duster","Green","5","2019","1"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
-peer chaincode invoke -C appchannel -n carcc -c '{"Args":["createCar","NO9279","Volvo","V90","White","5","2019","1"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
+peer chaincode invoke -C appchannel -n energycc -c '{"Args":["createUser","1", "ark", "2000", "solar panel, wind mill", "700", "Enegry at affordable price", "2","100", "50"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
+peer chaincode invoke -C appchannel -n energycc -c '{"Args":["createUser","2", "srk", "2500", "solar panel", "500", "Enegry at affordable price", "1","200", "100"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
+peer chaincode invoke -C appchannel -n energycc -c '{"Args":["createUser","3", "demo", "1000", "wind mill", "200", "Enegry at affordable price", "3","150", "200"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE} &&
+peer chaincode invoke -C appchannel -n energycc -c '{"Args":["createUser","4", "articonf", "3000", "solar panel, wind mill", "1000", "Enegry at affordable price", "1","300", "100"]}' --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
