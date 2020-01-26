17dd1d179147b277f6a91fc4c7f94c0ff4a92e22
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-01-26 19:32:11 +0100

    added fabcar sample

---
+++ fabcar/shell_scripts/instantiate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x #echo on
+
+export CORE_PEER_ADDRESS="peer2:7051"
+export CORE_PEER_MSPCONFIGPATH="/root/admin/msp"
+export CORE_PEER_TLS_ROOTCERT_FILE="/root/${PEER2_HOST}/tls-msp/tlscacerts/tls-${TLSCA_HOST}-7054.pem"
+export CORE_PEER_ADDRESS=$CORE_PEER_ADDRESS
+export CORE_PEER_MSPCONFIGPATH=$CORE_PEER_MSPCONFIGPATH
+export CORE_PEER_TLS_ROOTCERT_FILE=$CORE_PEER_TLS_ROOTCERT_FILE
+
+CHAINCODE_NAME="arkcar"
+peer chaincode instantiate -C appchannel -n $CHAINCODE_NAME -v 1.0 -c '{"Args":[init]}' -o ${ORDERER_HOST}:7050 --tls --cafile ${CORE_PEER_TLS_ROOTCERT_FILE}
