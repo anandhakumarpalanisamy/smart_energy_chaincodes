1310c9074a0eb3ba20559d69f73e9e46de884b0f
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-11-09 15:25:49 +0100

    - updated chaincodes

---
+++ transaction_chaincode/src/lib/chaincode.js
@@ -0,0 +1,154 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+"use strict";
+
+const { Contract } = require("fabric-contract-api");
+
+class TransactionChaincode extends Contract {
+  async InitLedger(ctx) {
+    const assets = [];
+    for (const asset of assets) {
+      asset.docType = "asset";
+      await ctx.stub.putState(asset.ID, Buffer.from(JSON.stringify(asset)));
+      console.info(`User ${asset.ID} initialized`);
+    }
+  }
+
+  // CreateAsset issues a new asset to the world state with given details.
+  async CreateAssetJson(ctx, assetJSON) {
+    // Parse json object
+    const assets = JSON.parse(assetJSON);
+
+    // If array of objects, loop through them
+    if (Array.isArray(assets)) {
+      for (const asset of assets) {
+        asset.docType = "asset";
+        await ctx.stub.putState(asset.ID, Buffer.from(JSON.stringify(asset)));
+        console.info(`Asset ${asset.ID} initialized`);
+      }
+    } else {
+      assets.docType = "asset";
+      await ctx.stub.putState(assets.ID, Buffer.from(JSON.stringify(assets)));
+      console.info(`Asset ${assets.ID} initialized`);
+    }
+  }
+
+  // ReadAsset returns the asset stored in the world state with given id.
+  async ReadAsset(ctx, id) {
+    const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+    if (!assetJSON || assetJSON.length === 0) {
+      throw new Error(`The user ${id} does not exist`);
+    }
+    return assetJSON.toString();
+  }
+
+  // UpdateAsset updates an existing asset in the world state with provided parameters.
+  async UpdateAsset(ctx, id, updateParamsJSON) {
+    const exists = await this.AssetExists(ctx, id);
+    if (!exists) {
+      throw new Error(`The user ${id} does not exist`);
+    }
+
+    // overwriting original asset with new asset
+    const updatedAsset = {
+      ID: id,
+      Balance: balance,
+      Type: type,
+    };
+    return ctx.stub.putState(id, Buffer.from(JSON.stringify(updatedAsset)));
+  }
+
+  // DeleteAsset deletes an given asset from the world state.
+  async DeleteAsset(ctx, id) {
+    const exists = await this.AssetExists(ctx, id);
+    if (!exists) {
+      throw new Error(`The user ${id} does not exist`);
+    }
+    return ctx.stub.deleteState(id);
+  }
+
+  // AssetExists returns true when asset with given ID exists in world state.
+  async AssetExists(ctx, id) {
+    const assetJSON = await ctx.stub.getState(id);
+    return assetJSON && assetJSON.length > 0;
+  }
+
+  // GetAsset returns the asset stored in the world state with given id.
+  async GetAsset(ctx, id) {
+    const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+    if (!assetJSON || assetJSON.length === 0) {
+      throw new Error(`The user ${id} does not exist`);
+    }
+    return assetJSON;
+  }
+
+  // GetAssetHistory returns the chain of custody for an asset since issuance.
+  async GetAssetHistory(ctx, assetName) {
+    let resultsIterator = await ctx.stub.getHistoryForKey(assetName);
+    let results = await this.GetAllResults(resultsIterator, true);
+
+    return JSON.stringify(results);
+  }
+
+  // GetAllAssets returns all assets found in the world state.
+  async GetAllAssets(ctx) {
+    const allResults = [];
+    // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
+    const iterator = await ctx.stub.getStateByRange("", "");
+    let result = await iterator.next();
+    while (!result.done) {
+      const strValue = Buffer.from(result.value.value.toString()).toString(
+        "utf8"
+      );
+      let record;
+      try {
+        record = JSON.parse(strValue);
+      } catch (err) {
+        console.log(err);
+        record = strValue;
+      }
+      allResults.push({ Key: result.value.key, Record: record });
+      result = await iterator.next();
+    }
+    return JSON.stringify(allResults);
+  }
+
+  async GetAllResults(iterator, isHistory) {
+    let allResults = [];
+    let res = await iterator.next();
+    while (!res.done) {
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString("utf8"));
+        if (isHistory && isHistory === true) {
+          jsonRes.TxId = res.value.tx_id;
+          jsonRes.Timestamp = res.value.timestamp;
+          try {
+            jsonRes.Value = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Value = res.value.value.toString("utf8");
+          }
+        } else {
+          jsonRes.Key = res.value.key;
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString("utf8"));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString("utf8");
+          }
+        }
+        allResults.push(jsonRes);
+      }
+      res = await iterator.next();
+    }
+    iterator.close();
+    return allResults;
+  }
+}
+
+module.exports = TransactionChaincode;
