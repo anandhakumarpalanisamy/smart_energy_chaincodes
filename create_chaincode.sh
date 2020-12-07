eaa0d034bc38025d2c46524648849dd76e611ef4
anandhakumarpalanisamy <anandhakumarpalanisamy91@gmail.com>
2020-12-07 13:36:34 +0100

    - updated create chaincode template

--- create_chaincode.sh
+++ create_chaincode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x #echo on
 ​
-CHAINCODE_NAME="transaction"
-CHAINCODE_CLASS_NAME="TransactionChaincode"
+CHAINCODE_NAME="advertisement"
+CHAINCODE_CLASS_NAME="AdvertisementChaincode"
 platform='unknown'
 
 git clone https://github.com/bityoga/fabric_chaincode_template.git $CHAINCODE_NAME
@@ -16,16 +16,18 @@ fi
 
 if [ "$platform" = 'linux' ]; then
 
+   find ./$CHAINCODE_NAME/chaincode/src/lib/META-INF/statedb/couchdb/indexes/AssetUtil.json -type f -exec sed -i "s/AssetUtil/$CHAINCODE_CLASS_NAME/g" {} \; && 
    find ./$CHAINCODE_NAME/chaincode/ -type f -exec sed -i "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \; && 
    find ./$CHAINCODE_NAME/chaincode/ -type f -exec sed -i "s/CHAINCODE_CLASS_NAME/$CHAINCODE_CLASS_NAME/g" {} \;
 
 elif [ "$platform" = 'freebsd' ]; then
-
+  find ./$CHAINCODE_NAME/chaincode/src/lib/META-INF/statedb/couchdb/indexes/AssetUtil.json -type f -exec sed -i '.bak' "s/AssetUtil/$CHAINCODE_CLASS_NAME/g" {} \;&& 
   find ./$CHAINCODE_NAME/chaincode/ -type f -exec sed -i '.bak' "s/CHAINCODE-NAME/$CHAINCODE_NAME/g" {} \;&&
   find ./$CHAINCODE_NAME/chaincode/ -type f -exec sed -i '.bak' "s/CHAINCODE_CLASS_NAME/$CHAINCODE_CLASS_NAME/g" {} \;
 
 fi
 
+mv ./$CHAINCODE_NAME/chaincode/src/lib/META-INF/statedb/couchdb/indexes/AssetUtil.json ./$CHAINCODE_NAME/chaincode/src/lib/META-INF/statedb/couchdb/indexes/index$CHAINCODE_CLASS_NAME.json 
 rm ./$CHAINCODE_NAME/create_chaincode.sh
 rm ./$CHAINCODE_NAME/.gitignore
 rm ./$CHAINCODE_NAME/LICENSE
